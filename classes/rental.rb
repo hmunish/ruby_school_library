c1c8ea9fcbba1ecf545dbc7d693fca50bed615d3
Munish <hmunish5@gmail.com>
2023-09-01 19:32:59 +0200

    Add basic UI

--- classes/rental.rb
+++ classes/rental.rb
@@ -5,7 +5,10 @@ class Rental
     @person = person
     @book = book
     @date = date
-    person.rental << self
+    person.rentals << self
     book.rental << self
+  end
+  def self.all
+    ObjectSpace.each_object(self).to_a
   end
 end
