e5294f80a548c2b0f07a0ca44c293ec870a4af36
Munish <hmunish5@gmail.com>
2023-09-01 10:54:40 +0200

    Add capitalize & trimmer decorator class

--- classes/person.rb
+++ classes/person.rb
@@ -1,4 +1,6 @@
 require_relative 'nameable'
+require_relative 'trimmer_decorator'
+require_relative 'capitalize_decorator'
 
 class Person < Nameable
   attr_reader :id
@@ -25,3 +27,10 @@ class Person < Nameable
     @age && @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
