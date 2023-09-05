19f081f80de8cc1fb4552de73c753e90b8dad6f3
Munish <hmunish5@gmail.com>
2023-09-05 10:34:35 +0200

    Add exe.rb to refactor code

--- classes/main.rb
+++ classes/main.rb
@@ -1,21 +1,23 @@
 require_relative 'app'
+require_relative 'exe'
 # rubocop:disable Metrics
 
-def display_ui
-  puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-  user_choice = gets.chomp.to_i
-  puts user_choice
-  execute(user_choice)
-end
+class Main
+  def display_ui
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    user_choice = gets.chomp.to_i
+    puts user_choice
+    Exe.new(user_choice).exe
+  end
 end
 # rubocop:enable Metrics
 
 puts 'Welcome in Ruby School Library App!'
-display_ui
+Main.new.display_ui
