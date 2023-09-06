c14e51e9e45eb4d070ff0fd60a9ce460a5c9b2b7
Munish <hmunish5@gmail.com>
2023-09-06 20:25:44 +0200

    Add test for correct_name methods on all classes

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -1,6 +1,7 @@
 require './classes/base_decorator'
 require './classes/capitalize_decorator'
 require './classes/trimmer_decorator'
+require './classes/person'
 
 describe Nameable do
   before :each do
@@ -21,26 +22,38 @@ describe Nameable do
 end
 
 describe BaseDecorator do
+  person = Person.new(25, 'Munish')
   context 'When testing BaseDecorator class' do
-    base_decorator = BaseDecorator.new('Jimmy')
+    base_decorator = BaseDecorator.new(person)
     it 'should create instance of base_decorator' do
       expect(base_decorator).to be_an_instance_of BaseDecorator
+    end
+    it 'should return correct name' do
+      expect(base_decorator.correct_name).to eq 'Munish'
     end
   end
 end
 describe CapitalizeDecorator do
+  person = Person.new(25, 'munish')
   context 'When testing CapitalizeDecorator class' do
-    capitalize_decorator = CapitalizeDecorator.new('Jimmy')
+    capitalize_decorator = CapitalizeDecorator.new(person)
     it 'should create instance of capitalize_decorator' do
       expect(capitalize_decorator).to be_an_instance_of CapitalizeDecorator
+    end
+    it 'should return correct name' do
+      expect(capitalize_decorator.correct_name).to eq 'Munish'
     end
   end
 end
 describe TrimmerDecorator do
+  person = Person.new(25, 'Munish Hanief Halai')
   context 'When testing TrimmerDecorator class' do
-    trimmer_decorator = TrimmerDecorator.new('Jimmy')
+    trimmer_decorator = TrimmerDecorator.new(person)
     it 'should create instance of trimmer_decorator' do
       expect(trimmer_decorator).to be_an_instance_of TrimmerDecorator
+    end
+    it 'should return correct name' do
+      expect(trimmer_decorator.correct_name).to eq 'Munish Han'
     end
   end
 end
