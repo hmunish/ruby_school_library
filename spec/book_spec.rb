d61d239ebbd868573ac4e35d4758468a3520215d
Munish <hmunish5@gmail.com>
2023-09-06 19:48:17 +0200

    Add test for Book class methods

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,4 +1,6 @@
 require './classes/book'
+require './classes/person'
+require './classes/rental'
 
 describe Book do
   context 'When testing the Book class' do
@@ -14,6 +16,15 @@ describe Book do
 
     it 'should create instance with correct author name' do
       expect(book.author).to eq 'J.K.Rowlings'
+    end
+
+    it 'should add rental' do
+      book.add_rental('2023-06-09', Person.new(25, 'Munish'))
+      expect(book.rental[0]).to be_an_instance_of Rental
+    end
+
+    it 'should return all instances of Book' do
+      expect(Book.all[0]).to be_an_instance_of Book
     end
   end
 end
